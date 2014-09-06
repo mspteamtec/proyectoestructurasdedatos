2b454db4cba2db4033b94d458859ccbb95396b40
Daryn <darynsoto22@gmail.com>
2014-09-05 21:04:14 -0600

    Avance 3
    vea todo :+1:

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <stdlib.h>
+#include <cstring>
 
 using namespace std;
 
+/************************************************************************/
+
+//Estructura de la lista Carrera
+struct Carreras
+{
+    string carrera; // La variable para guardar el nombre de la Carrera
+    struct Carreras *sig; //Puntero al siguiente nodo de la lista
+    struct Cursos *PrimeroCurso;//Puntero que enlaza con Curso
+}ListaCarreras;//La variable global para guardar las Carreras
+
+/************************************************************************/
+
+//Estructura de la Lista Cursos
+struct Cursos
+{
+    string codigo;// La variable para guardar el codigo
+    string materia;// La variable para guardar el nombre del curso
+    int grupo;// La variable para guardar el numero de grupo que va tener el Curso
+    int creditos;// La variable para guardar la cantidad de creditos que va tener el Curso
+    string horario;// La variable para guardar el horario del Curso
+    struct Profesor *PrimeroProfesor;// Puntero que enlaza con Profesor
+}ListaCursos;// La variable global para guardar los Cursos
+
+/************************************************************************/
+
+
+
+
+
 int main()
 {
-    cout << "Hola Dario gay" << endl;
+    cout << "Cristian Playo" << endl;
     return 0;
 }
