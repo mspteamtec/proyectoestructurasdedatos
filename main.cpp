97147ef2b0829715ac346ef18118e95d042d40c0
Daryn <darynsoto22@gmail.com>
2014-09-05 21:27:52 -0600

    Avance 4
    ak7 :+1:

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /************************************************************************/
 
-//Estructura de la lista Carrera
+//Estructura de la lista Carrera simple
 struct Carreras
 {
     string carrera; // La variable para guardar el nombre de la Carrera
@@ -16,16 +16,29 @@ struct Carreras
 
 /************************************************************************/
 
-//Estructura de la Lista Cursos
+//Estructura de la Lista Cursos doble
 struct Cursos
 {
     string codigo;// La variable para guardar el codigo
     string materia;// La variable para guardar el nombre del curso
     int grupo;// La variable para guardar el numero de grupo que va tener el Curso
     int creditos;// La variable para guardar la cantidad de creditos que va tener el Curso
+    string restriccion;// La variable para guardar las restricciones
     string horario;// La variable para guardar el horario del Curso
+    struct Cursos *sig, *ant;// Los punteros antes y siguiente
     struct Profesor *PrimeroProfesor;// Puntero que enlaza con Profesor
 }ListaCursos;// La variable global para guardar los Cursos
+
+/************************************************************************/
+
+//Estructura de la Lista Profesor simple
+struct Profesor
+{
+    string nombre;// La variable para guardar el nombre del Profesor
+    string restriccion;// La variable para guardar las restricciones del Profesor
+    struct Profesor *sig;// Puntero siguiente
+}ListaProfesor;
+
 
 /************************************************************************/
 
